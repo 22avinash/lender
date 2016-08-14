d74c42ca335e7c48d1609eccc5f1e2e01f487639
Avinash <arai08230@gmail.com>
2016-08-14 19:10:45 +0530

    login code complete

--- App/app/src/main/java/com/example/avinash/app/LoginActivity.java
+++ App/app/src/main/java/com/example/avinash/app/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.example.avinash.app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +11,15 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import async.AsyncResult;
 import async.LoginAsync;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements AsyncResult {
 
     TextView txtStatus;
+    Context context;
+    SharedPreferences prefs;
+    AsyncResult listener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +29,34 @@ public class LoginActivity extends AppCompatActivity {
         final TextView txtUsr = (TextView) findViewById(R.id.txtUsr);
         final TextView txtPwd = (TextView) findViewById(R.id.txtPwd);
         txtStatus = (TextView) findViewById(R.id.txtStatus);
+        context = getApplicationContext();
+        prefs = getSharedPreferences("MyPrefs",MODE_PRIVATE);
+        checkLogin();
+        if(this instanceof AsyncResult)
+            listener = (AsyncResult)this;
+
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                LoginAsync asyncLogin = new LoginAsync(getApplicationContext(),txtStatus);
+                LoginAsync asyncLogin = new LoginAsync(getApplicationContext(),listener,txtStatus);
                 String usr = ""+txtUsr.getText();
                 String pwd = ""+txtPwd.getText();
                 asyncLogin.execute(usr,pwd);
             }
         });
+    }
+    public void checkLogin(){
+        if(prefs.contains("usr")) {
+            Intent mainIntent = (Intent) new Intent(context, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onComplete() {
+        checkLogin();
     }
 }
